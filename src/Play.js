f3cf670cbc5cbcd855a187130928948b2ce91bcb
Astra Tsai <astra@ucsc.edu>
2025-02-09 14:44:01 -0800

    Implement a Graphics class for immediate mode text rendering

--- src/Play.js
+++ src/Play.js
@@ -6,12 +6,17 @@ const Game = class {
         this.synth = synth
         this.terrain = new TerrainGen()
     }
-    render() {
+    render(time, delta) {
+        this.cx.startFrame()
         let x = this.terrain.pullSheet()
         for (let e of x) {
             this.cx.fillStyle(e.color)
             this.cx.fillRect(e.x, e.y, e.width, e.height)
         }
+
+        this.cx.fillStyle(0xffffff)
+        this.cx.fillText(100, 200 + Math.sin(time * Math.PI) * 100, { height: 50 }, 'hello')
+        this.cx.endFrame()
     }
     mouseDown() {
 
@@ -30,13 +35,74 @@ const Game = class {
     }
 }
 
+const Graphics = class extends Phaser.GameObjects.Graphics {
+    constructor(scene) {
+        super(scene)
+        scene.add.displayList.add(this)
+        this.width = scene.game.config.width
+        this.height = scene.game.config.height
+        this.cachedText = new Map()
+    }
+    startFrame() {
+        this.clear()
+        for (let cache of this.cachedText.values()) {
+            cache.active = 0
+        }
+    }
+    fillStyle(color, alpha = 1) {
+        this.cachedFillStyle = { color, alpha }
+        super.fillStyle(color, alpha)
+    }
+    fillText(x, y, { align = 'center', width = 0, height }, text) {
+        let { color, alpha } = this.cachedFillStyle
+
+        let cacheKey = [color, height, text].join(' ')
+        let cache = this.cachedText.get(cacheKey)
+        if (cache == null) {
+            cache = { objects: [], active: 0 }
+            this.cachedText.set(cacheKey, cache)
+        }
+
+        let object
+        if (cache.objects.length - cache.active === 0) {
+            object = this.scene.add.text(x, y, text, {
+                align,
+                color: `#${color.toString(16).padStart(6, '0')}`,
+                fontFamily: 'Ubuntu Titling',
+                fontSize: height,
+            })
+            cache.objects.push(object)
+        } else {
+            object = cache.objects[cache.active]
+            object.x = x
+            object.y = y
+            object.text = text
+        }
+        cache.active++
+
+        object.setAlign(align)
+        object.setAlpha(alpha)
+        object.setFixedSize(width, 0)
+    }
+    endFrame() {
+        for (let [key, cache] of this.cachedText.entries()) {
+            while (cache.objects.length > cache.active) {
+                cache.objects.pop().destroy()
+            }
+            if (cache.active === 0) {
+                this.cachedText.delete(key)
+            }
+        }
+    }
+}
+
 export const Play = class extends Phaser.Scene {
     constructor() {
         super('Play')
     }
 
     create() {
-        this.graphics = this.add.graphics()
+        this.graphics = new Graphics(this)
         this.synth = new Synth(this.sound.context)
         this.gameLogic = new Game(this.graphics, this.synth)
 
@@ -47,7 +113,7 @@ export const Play = class extends Phaser.Scene {
         this.input.keyboard.on('keyup', e => this.gameLogic.keyUp(e.key))
     }
 
-    update() {
-        this.gameLogic.render()
+    update(time, delta) {
+        this.gameLogic.render(time / 1000, delta / 1000)
     }
 }
